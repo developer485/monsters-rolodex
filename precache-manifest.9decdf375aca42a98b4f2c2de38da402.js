224bb0f25f70da6144595d545bb34155aa273c75
developer485 <asram517@gmail.com>
2020-02-28 08:52:16 +0530

    Updates

---
+++ precache-manifest.9decdf375aca42a98b4f2c2de38da402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987fc430436168370b7de0e8fe90f378",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38cfe705cbd50f398e2a",
+    "url": "/monsters-rolodex/static/css/main.c35224e2.chunk.css"
+  },
+  {
+    "revision": "a85965cfb6027a00e7be",
+    "url": "/monsters-rolodex/static/js/2.2de65f35.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.2de65f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cfe705cbd50f398e2a",
+    "url": "/monsters-rolodex/static/js/main.c89cbcbe.chunk.js"
+  },
+  {
+    "revision": "69e349d70d8164ea7a7a",
+    "url": "/monsters-rolodex/static/js/runtime-main.0be9ec98.js"
+  }
+]);
